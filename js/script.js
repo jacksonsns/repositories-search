61a2277d0f0d1fd9997c2df8725fa0724e36e560
jacksonsns <jack-g3@hotmail.com>
2023-09-23 12:44:11 -0300

    refactor: refactoring the html and script

--- js/script.js
+++ js/script.js
@@ -1,60 +1,88 @@
-const content = document.querySelector('#content');
-const input = document.querySelector('#input');
+const content = document.querySelector('#repository-table-body');
 const btn = document.querySelector('#btn');
+const input = document.querySelector('#input');
 
 let username = null;
 
+function renderRepositories(repositories) {
+    content.innerHTML = '';
 
-function getRepository(repositories) {     
-     
-     axios
+    if (repositories.length === 0) {
+        const noRepoRow = document.createElement('tr');
+        const noRepoCell = document.createElement('td');
+        noRepoCell.setAttribute('colspan', '4');
+        noRepoCell.textContent = 'Nenhum repositório encontrado para este usuário.';
+        noRepoRow.appendChild(noRepoCell);
+        content.appendChild(noRepoRow);
+    } else {
+        repositories.forEach(({ name, description, language, stargazers_count }) => {
+            const row = document.createElement('tr');
 
-     .get(`https://api.github.com/users/${username}/repos`)
+            const nameCell = document.createElement('td');
+            const nameLink = document.createElement('a');
+            nameLink.href = `https://github.com/${username}/${name}`;
+            nameLink.target = '_blank';
+            nameLink.textContent = name;
+            nameCell.appendChild(nameLink);
 
-     .then((response) => {
-        
-        const repositories = response.data;      
+            const descriptionCell = document.createElement('td');
+            descriptionCell.textContent = description || 'N/A';
 
-        if (repositories.length === 0) {
+            const languageCell = document.createElement('td');
+            languageCell.textContent = language || 'N/A';
 
-            alert("User not found!");
+            const starsCell = document.createElement('td');
+            starsCell.textContent = stargazers_count || '0';
 
-        } else {
+            [nameCell, descriptionCell, languageCell, starsCell].forEach(cell => {
+                row.appendChild(cell);
+            });
 
-            repositories.forEach(({ name }) => {
-                const link = document.createElement('a');
-                link.setAttribute('href', `https://github.com/${username}/${name}`);
-                link.setAttribute('target', '_blank');
-                link.innerHTML = `${name}`;
-                content.appendChild(link);
-            }); 
-        }
-
-        return repositories ;
-
-        
-     })
-
-     .catch((error) => {
-
-        console.log(error); 
-       
-      }); 
+            content.appendChild(row);
+        });
+    }
 }
 
-function submit(event) { 
-  if (event.key === "Enter" || event.type === "click") {
-    username = input.value;
-    console.log(username); 
+function getRepository() {
+    axios
+        .get(`https://api.github.com/users/${username}/repos`)
+        .then((response) => {
+            if (response.status === 200) {
+                const repositories = response.data;
+                renderRepositories(repositories);
+            } else {
+                content.innerHTML = '';
+                const errorRow = document.createElement('tr');
+                const errorCell = document.createElement('td');
+                errorCell.setAttribute('colspan', '4');
+                errorCell.textContent = 'Erro ao acessar a API do GitHub.';
+                errorRow.appendChild(errorCell);
+                content.appendChild(errorRow);
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            content.innerHTML = '';
+            const errorRow = document.createElement('tr');
+            const errorCell = document.createElement('td');
+            errorCell.setAttribute('colspan', '4');
+            errorCell.textContent = 'Erro ao acessar a API do GitHub.';
+            errorRow.appendChild(errorCell);
+            content.appendChild(errorRow);
+        });
+}
 
-    if (username === "") {
-            alert("Enter the username github!");
+function submit(event) {
+    if (event.key === "Enter" || event.type === "click") {
+        username = input.value.trim();
+        console.log(username);
+
+        if (username === "") {
+            alert("Digite o nome de usuário do GitHub!");
         } else {
-            content.innerHTML = '';  
-            getRepository(); 
-        }  
-
-  }
+            getRepository();
+        }
+    }
 }
 
 btn.addEventListener("click", submit);
